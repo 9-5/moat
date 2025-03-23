e19cb4da5009ac633b9df36eafecf7d9ece0ea12
John Le <john@johnle.org>
2025-03-23 10:09:07 -0400

    Add database initialization and first user creation on server startup

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+import typer
+import uvicorn
+import asyncio
 
-app = FastAPI()
+from moat import server, config, database, models
+app_cli = typer.Typer()
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app_cli.command()
+def run(reload: bool = False):
+    """
+    Run the Moat server.
+    """
+    asyncio.run(database.init_db()) # Initialize database
+    uvicorn.run("moat.server:app", host="0.0.0.0", port=8000, reload=reload)
+
+if __name__ == "__main__":
+    app_cli()
